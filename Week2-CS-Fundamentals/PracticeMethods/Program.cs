6d703fbf494e230957c0682ca65855bf4884a680
johannasmithy <johannaknoblauchsmith@gmail.com>
2024-04-25 16:32:30 -0400

    updating methods practice

--- Week2-CS-Fundamentals/PracticeMethods/Program.cs
+++ Week2-CS-Fundamentals/PracticeMethods/Program.cs
@@ -21,39 +21,63 @@ class Program
         System.Console.WriteLine("You entered: " + word1 + ". Please enter your second word:");
         string word2 = Console.ReadLine();
         System.Console.WriteLine("You entered: " + word2 + ".");
-        System.Console.WriteLine(word1 + " " + word2 + " " + word1 + " " + word2);
+        for(var i=0; i<3; i++){
+            System.Console.WriteLine(word1 + " " + word2);
+        }
+
+        
     }
 
     public static void LargestInt()
     {
+        int[] input = new int[3];
         System.Console.WriteLine("Please enter a number between 1 and 100: ");
-        string int1 = Console.ReadLine();
-        int firstInt = 0;
-        if(int1 != null) firstInt = int.Parse(int1);
-        System.Console.WriteLine("You entered " + firstInt + ". Please enter a different number between 1 and 100:");
-        string int2 = Console.ReadLine();
-        int secondInt = 0;
-        if(int2 != null) secondInt = int.Parse(int2);
-        System.Console.WriteLine("You entered " + secondInt + ". Please enter another different number between 1 and 100:");
-        string int3 = Console.ReadLine();
-        int thirdInt = 0;
-        if(int3 != null) thirdInt = int.Parse(int3);
-        System.Console.WriteLine("You entered " + thirdInt + ".");
-        if (firstInt > secondInt)
-        if (firstInt > thirdInt)
-        {
-            System.Console.WriteLine("The largest number is: " + firstInt + ".");
+        for(int i=0; i<3; i++){
+            
+            input[i] = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("You entered " + input[i] + ". Please enter a different number between 1 and 100:");
         }
-        if (secondInt > firstInt)
-        if (secondInt > thirdInt)
+        
+        // int firstInt = 0;
+        // if(input[0] != null) firstInt = int.Parse(input[0]);
+        
+        // input[1] = Console.ReadLine();
+        // int secondInt = 0;
+        // if(input[1] != null) secondInt = int.Parse(input[1]);
+        // System.Console.WriteLine("You entered " + secondInt + ". Please enter another different number between 1 and 100:");
+        // input[2] = Console.ReadLine();
+        // int thirdInt = 0;
+        // if(input[2] != null) thirdInt = int.Parse(input[2]);
+        // System.Console.WriteLine("You entered " + thirdInt + ".");
+
+
+        // if (input[0] > input[1])
+        // {
+        //     if (input[0] > input[2])
+        //         {
+        //             System.Console.WriteLine("The largest number is: " + input[0] + ".");
+        //         }
+        // }
+        
+        // if (input[1] > input[0])
+        // if (input[1] > input[2])
+        // {
+        //     System.Console.WriteLine("The largest number is: " + input[1] + ".");
+        // }
+        // if (input[2] > input[0])
+        // if (input[2] > input[1])
+        // {
+        //     System.Console.WriteLine("The largest number is: " + input[2] + ".");
+        // }
+
+        var maxValue = 0;
+
+        foreach (var num in input)
         {
-            System.Console.WriteLine("The largest number is: " + secondInt + ".");
+            if (num > maxValue)
+                maxValue = num;
         }
-        if (thirdInt > firstInt)
-        if (thirdInt > secondInt)
-        {
-            System.Console.WriteLine("The largest number is: " + thirdInt + ".");
-        }
+        System.Console.WriteLine("The largest number is: " + maxValue);
     }
 }
 
